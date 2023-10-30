1792364f3ed97d7ca451314656c641434cf80e44
VGAliyev <v.aliev84@yandex.ru>
2023-10-30 20:46:34 +0400

    Initial commit

---
+++ src/test/java/pro/sky/course2/aliev/emloyeebook/EmloyeebookApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.aliev.emloyeebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmloyeebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
